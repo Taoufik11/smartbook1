93f9ee05eeabe3faa28cc6523e9b6ef2c749eb98
Astra-001 <indianarmy2625@gmail.com>
2020-11-16 16:54:59 -0800

    dty

--- PHP/config.php
+++ PHP/config.php
@@ -3,5 +3,5 @@ $DNAME="stpxvtzxhp" ;
 $PASS= "Book@2625";
 $ADMIN= "stpxvtzxhp";
 $SName = "78.141.199.9";
-$conn = mysqli_connect($SName,$ADMIN,$PASS, $DNAME);
+$conn = mysqli_connect($SName,$ADMIN,$PASS, $DNAME) or die("error in connection");
 ?>
